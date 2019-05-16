34498284c077a388081944e5606d522e5fe9bcdf
Brian.Bian <Brian.Xb.Bian@gmail.com>
2019-05-16 15:28:06 +0800

    Refactor strain

--- elixir/strain/strain.exs
+++ elixir/strain/strain.exs
@@ -5,26 +5,31 @@ defmodule Strain do
 
   Do not use `Enum.filter`.
   """
+  # Original
+  # @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  # def keep(list, fun) do
+  #   list
+  #   |> get_filter(fun)
+  #   |> do_filter(list, [])
+  # end
+
+  # def get_filter(list, fun) do
+  #   for x <- list, do: apply(fun, [x])
+  # end
+
+  # def do_filter([], [], filtered), do: filtered
+
+  # def do_filter([f_h | filter], [l_h | list], filtered) do
+  #   if f_h do
+  #     do_filter(filter, list, filtered ++ [l_h])
+  #   else
+  #     do_filter(filter, list, filtered)
+  #   end
+  # end
+
+  # Better
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def keep(list, fun) do
-    list
-    |> get_filter(fun)
-    |> do_filter(list, [])
-  end
-
-  def get_filter(list, fun) do
-    for x <- list, do: apply(fun, [x])
-  end
-
-  def do_filter([], [], filtered), do: filtered
-
-  def do_filter([f_h | filter], [l_h | list], filtered) do
-    if f_h do
-      do_filter(filter, list, filtered ++ [l_h])
-    else
-      do_filter(filter, list, filtered)
-    end
-  end
+  def keep(list, fun), do: for(x <- list, fun.(x), do: x)
 
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
@@ -32,8 +37,13 @@ defmodule Strain do
 
   Do not use `Enum.reject`.
   """
+  # Original
+  # @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  # def discard(list, fun) do
+  #   list -- keep(list, fun)
+  # end
+
+  # Better
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def discard(list, fun) do
-    list -- keep(list, fun)
-  end
+  def discard(list, fun), do: for(x <- list, not fun.(x), do: x)
 end

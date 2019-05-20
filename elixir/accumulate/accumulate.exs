e029d6a3b8cdcb035b3de27297c6d29cd6f60778
Brian.Bian <Brian.Xb.Bian@gmail.com>
2019-05-20 15:30:14 +0800

    Add accumulate

---
+++ elixir/accumulate/accumulate.exs
@@ -0,0 +1,30 @@
+defmodule Accumulate do
+  @doc """
+    Given a list and a function, apply the function to each list item and
+    replace it with the function's return value.
+
+    Returns a list.
+
+    ## Examples
+
+      iex> Accumulate.accumulate([], fn(x) -> x * 2 end)
+      []
+
+      iex> Accumulate.accumulate([1, 2, 3], fn(x) -> x * 2 end)
+      [2, 4, 6]
+
+  """
+
+  @spec accumulate(list, (any -> any)) :: list
+  def accumulate(list, fun) do
+    do_accumulate(list, fun, [])
+  end
+
+  defp do_accumulate([], _fun, result) do
+    result |> Enum.reverse()
+  end
+
+  defp do_accumulate([h | t], fun, result) do
+    do_accumulate(t, fun, [fun.(h) | result])
+  end
+end

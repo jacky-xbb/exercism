6322f7b6719365fee462840075a78d58ca78a576
Brian.Bian <Brian.Xb.Bian@gmail.com>
2019-05-27 11:19:41 +0800

    Refactor sublist

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,26 +3,16 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, b) when length(a) == length(b) do
-    case a === b do
-      true -> :equal
-      false -> :unequal
+  def compare(a, b) do
+    cond do
+      equals?(a, b) -> :equal
+      contains?(a, b) -> :superlist
+      contains?(b, a) -> :sublist
+      true -> :unequal
     end
   end
 
-  def compare(a, b) when length(a) > length(b) do
-    case contains?(a, b) do
-      true -> :superlist
-      false -> :unequal
-    end
-  end
-
-  def compare(a, b) when length(a) < length(b) do
-    case contains?(b, a) do
-      true -> :sublist
-      false -> :unequal
-    end
-  end
+  defp equals?(a, b), do: a === b
 
   defp contains?(a, b) when length(a) < length(b), do: false
 

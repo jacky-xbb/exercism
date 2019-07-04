f361c6a67ff70c3adefed4799bf1e5d7955971d5
Brian.Bian <Brian.Xb.Bian@gmail.com>
2019-07-04 14:46:23 +0800

    Update bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,47 +1,26 @@
 defmodule Bob do
-
-
-   def hey(phrase) do
+  def hey(input) do
     cond do
-      is_silence?(phrase) ->
-        "Fine. Be that way!"
-      is_ask_in_capitals?(phrase) && is_question?(phrase) ->
+      has_letters?(input) and uppercased?(input) and String.ends_with?(input, "?") ->
         "Calm down, I know what I'm doing!"
-      is_shouting_numbers?(phrase) || is_russian?(phrase) || is_shouting_with_uppercase?(phrase) ->
-        "Whoa, chill out!"
-      is_question?(phrase) ->
+
+      String.ends_with?(input, "?") ->
         "Sure."
+
+      String.trim(input) == "" ->
+        "Fine. Be that way!"
+
+      has_letters?(input) and uppercased?(input) ->
+        "Whoa, chill out!"
+
       true ->
         "Whatever."
     end
   end
 
-  defp is_silence?(phrase), do: String.trim(phrase) == ""
-  defp is_ask_in_capitals?(phrase) do
-    phrase
-    |> String.replace(~r/\?$/, "")
-    |> String.split()
-    |> Enum.all?(fn x -> String.match?(x, ~r/^[A-Z]/) end)
-  end
-  defp is_russian?(phrase) do
-    phrase
-    |> String.match?(~r/[а-яА-Я]+/)
-  end
-  defp is_shouting_numbers?(phrase) do
-    String.match?(phrase, ~r/[0-9]/) && String.ends_with?(phrase, "!")
-  end
-  defp is_shouting_with_uppercase?(phrase) do
-    case String.ends_with?(phrase, "!") do
-      true ->
-        phrase |> String.trim_trailing("!") |> String.match?(~r/^[A-Z\s]+$/)
-      false ->
-        String.match?(phrase, ~r/^[A-Z\s]+$/)
-    end
-  end
-  defp is_question?(phrase), do: String.ends_with?(phrase, "?")
+  defp has_letters?(input),
+    do: String.upcase(input) != String.downcase(input)
+
+  defp uppercased?(input),
+    do: String.upcase(input) == input
 end
-
-
-
-
-
